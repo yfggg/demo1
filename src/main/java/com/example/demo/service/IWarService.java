d06fe03a557eaee1d092b5463394069d520052ef
yangf <497302152@qq.com>
2022-01-17 17:09:02 +0800

    第二张表

---
+++ src/main/java/com/example/demo/service/IWarService.java
@@ -0,0 +1,16 @@
+package com.example.demo.service;
+
+import com.example.demo.entity.War;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author yangfan
+ * @since 2022-01-17
+ */
+public interface IWarService extends IService<War> {
+
+}
